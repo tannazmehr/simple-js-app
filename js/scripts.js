af4fb1da31ba2d973b29e487788784f1372bcbba
tannazmehr <tannazmehrjooee@gmail.com>
2024-11-11 14:14:58 +0100

    created an array of Pokémons

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
-
-let favoriteFood = 'Lasagna' ;
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Nidoqueen' , height:1.3 , types: ['GROUND', 'POISEN']},
+    {name: 'Omanyte' , height:0.4 , types: ['WATER', 'ROCK']},
+    {name: 'Weavile' , height:1.1 , types: ['DARK', 'ICE']},
+    {name: 'Crobat' , height:1.8 , types: ['FLYING', 'POISEN']},
+    {name: 'Pignite' , height:1.0 , types: ['FIRE', 'FIGHTING']}
+];
