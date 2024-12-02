c9d0b384de713ff1536140868519985966842956
tannazmehr <tannazmehrjooee@gmail.com>
2024-12-02 13:58:09 +0100

    printed the all Pokemen names with their height

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,21 @@ let pokemonList = [
     {name: 'Crobat' , height:1.8 , types: ['FLYING', 'POISEN']},
     {name: 'Pignite' , height:1.0 , types: ['FIRE', 'FIGHTING']}
 ];
+
+
+//printing the name and the height of the Pokemons
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<p>" + `${pokemonList[i].name} (H:${pokemonList[i].height})` + "</p>")
+}
+
+
+
+for (let i = 0; i < pokemonList.length; i++) {
+    if (pokemonList[i].height > 1.5) {
+        label = 'You are the biggest Pokemon!'
+    }else{
+        label=''
+    }
+    document.write("<p>" + `${pokemonList[i].name} (H:${pokemonList[i].height}) ${label}` + "</p>")
+}
